e8140d226f367fcdc25acf95bf226cc4bebea099
Eduar <educartoons@gmail.com>
2020-06-07 19:36:49 -0500

    remove unused vars

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,10 @@
 const {
   override, 
-  addBabelPlugin, 
   getBabelLoader, 
-  addWebpackPlugin, 
   addWebpackModuleRule
 } = require('customize-cra');
 
-const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-
 module.exports = (config, env) => {
-  const prod = config.mode === 'production';
   const babelLoader = getBabelLoader(config);
   
   return override(
